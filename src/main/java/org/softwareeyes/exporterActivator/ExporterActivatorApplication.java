f3580f08aefe1e908566bbb6d1837591d6884b2a
Roeei itshayek <roeeiit1@gmail.com>
2023-03-16 21:18:31 +0200

    fix: removed unused imports

--- src/main/java/org/softwareeyes/exporterActivator/ExporterActivatorApplication.java
+++ src/main/java/org/softwareeyes/exporterActivator/ExporterActivatorApplication.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
--- src/main/java/org/softwareeyes/exporterActivator/activator/ExporterActivator.java
+++ src/main/java/org/softwareeyes/exporterActivator/activator/ExporterActivator.java
@@ -3,19 +3,13 @@ package org.softwareeyes.exporterActivator.activator;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import org.softwareeyes.common.clients.SoftwareEyesMongoClient;
-import org.softwareeyes.common.exporterConfiguration.implementation.mongodb.MongoDbExporterConfigurationParameter;
 import org.softwareeyes.common.models.exporterConfiguration.ExporterConfiguration;
-import org.softwareeyes.common.models.exporterConfiguration.ExporterMetadata;
-import org.softwareeyes.common.models.exporterConfiguration.exporterTopology.ExporterTopology;
-import org.softwareeyes.common.models.exporterConfiguration.exporterTopology.TopologyData;
-import org.softwareeyes.common.models.exporterConfiguration.exporterTopology.TopologyPosition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Component
 public class ExporterActivator {
