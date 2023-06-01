b09b03689cc029e0242872304e60d1c677e05481
Roeei itshayek <roeeiit1@gmail.com>
2023-06-01 20:37:41 +0300

    added AWS Function creation support

--- src/main/java/org/softwareeyes/exporterActivator/activator/ExporterActivator.java
+++ src/main/java/org/softwareeyes/exporterActivator/activator/ExporterActivator.java
@@ -7,6 +7,11 @@ import org.softwareeyes.common.models.exporterConfiguration.ExporterConfiguratio
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.services.lambda.LambdaClient;
+import software.amazon.awssdk.services.lambda.model.*;
+import software.amazon.awssdk.services.lambda.waiters.LambdaWaiter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,15 +24,19 @@ public class ExporterActivator {
     private String databaseName;
     @Value("${exporter.activator.collectionName}")
     private String collectionName;
+    @Value("${exporter.activator.scopeName}")
+    private String scopeName;
+
+    private final LambdaClient lambdaClient;
 
     public ExporterActivator() {
-
+        lambdaClient = LambdaClient.builder().credentialsProvider(EnvironmentVariableCredentialsProvider.create()).build();
     }
 
     @Scheduled(initialDelay = 1, fixedDelay = 5000)
     public void scanExporters() throws Exception {
-        try (SoftwareEyesMongoClient exportersClient = new SoftwareEyesMongoClient(this.connectionString, this.databaseName, this.collectionName)) {
-            MongoCollection<Document> collection = exportersClient.getQuerier();
+        try (SoftwareEyesMongoClient exportersClient = new SoftwareEyesMongoClient(this.connectionString, this.databaseName)) {
+            MongoCollection<Document> collection = exportersClient.getQuerier(this.collectionName);
             List<ExporterConfiguration> exporterConfigurations = collection.find(new Document(), ExporterConfiguration.class).into(new ArrayList<>());
             for (ExporterConfiguration configuration : exporterConfigurations) {
                 activateExporter(configuration);
@@ -37,11 +46,19 @@ public class ExporterActivator {
         }
     }
 
-    private void clearExporters(){
-        
+    private void clearExporters() {
+
     }
 
     private void activateExporter(ExporterConfiguration exporterConfiguration) {
-        // TODO: implement exporter activation using terraform
+        LambdaWaiter waiter = lambdaClient.waiter();
+        CreateFunctionRequest functionRequest = exporterConfiguration.CreateLambdaFunction();
+        String functionName = functionRequest.functionName();
+        CreateFunctionUrlConfigRequest createFunctionUrlConfigRequest = CreateFunctionUrlConfigRequest.builder().functionName(functionName).authType(FunctionUrlAuthType.NONE).build();
+        CreateFunctionResponse functionResponse = lambdaClient.createFunction(functionRequest);
+        GetFunctionRequest getFunctionRequest = GetFunctionRequest.builder().functionName(functionName).build();
+        waiter.waitUntilFunctionExists(getFunctionRequest);
+        CreateFunctionUrlConfigResponse functionUrlResponse = lambdaClient.createFunctionUrlConfig(createFunctionUrlConfigRequest);
+        System.out.println("The function URL is " + functionUrlResponse.functionUrl());
     }
 }
